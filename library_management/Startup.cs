60a71abfabb815a4595698e86d9a06a90850928a
yachnytskyi <yachnytskyi1992@gmail.com>
2019-01-23 05:13:56 -0800

    Added in Startup.cs LibraryDbContext, ICustomerRepository IAuthorRepository and IBookRepository Interfaces

--- library_management/Startup.cs
+++ library_management/Startup.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using library_management.Data;
+using library_management.Data.Interfaces;
+using library_management.Data.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -32,6 +36,10 @@ namespace library_management
             });
 
 
+            services.AddDbContext<LibraryDbContext>(options => options.UseInMemoryDatabase("LibraryContext"));
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddTransient<IAuthorRepository, AuthorRepository>();
+            services.AddTransient<IBookRepository, BookRepository>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
