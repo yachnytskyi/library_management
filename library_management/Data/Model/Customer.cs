956d31b273908148ee63633fce35bbf10454cb2a
yachnytskyi <yachnytskyi1992@gmail.com>
2019-01-23 01:09:56 -0800

    Added Models Author, Book and Customer

---
+++ library_management/Data/Model/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace library_management.Data.Model
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+    }
+}
