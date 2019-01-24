8145a096d32ed322a961004dec0f118be0a0319d
yachnytskyi <yachnytskyi1992@gmail.com>
2019-01-24 04:25:06 -0800

    Added Views/Customer/ Create, Update and Delete Views, and added Create, Update and Delete methods to CustomerController ( successfuly basic CRUD interface for Customer

--- library_management/Controllers/CustomerController.cs
+++ library_management/Controllers/CustomerController.cs
@@ -42,5 +42,42 @@ namespace library_management.Controllers
 
             return View(customerVM);
         }
+
+        public IActionResult Delete(int id)
+        {
+            var customer = _customerRepository.GetById(id);
+
+            _customerRepository.Delete(customer);
+
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            _customerRepository.Create(customer);
+
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Update(int id)
+        {
+            var customer = _customerRepository.GetById(id);
+
+            return View(customer);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Customer customer)
+        {
+            _customerRepository.Update(customer);
+
+            return RedirectToAction("List");
+        }
     }
 }
